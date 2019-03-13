c14bc6bcd20a147744ef579b5cb7ff57674fd42e
krishvla <pothalavamsi@gmail.com>
2019-03-13 17:35:37 +0530

    make it better

--- upgrad_project/settings.py
+++ upgrad_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from celery.schedules import crontab
+from datetime import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,6 +95,11 @@ DATABASES = {
 
     }
 }
+
+t = datetime.today()
+ONE_MINUTE_AFTER = crontab(minute=t.minute + 1, hour=t.hour)
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
+
 
 
 # Password validation
